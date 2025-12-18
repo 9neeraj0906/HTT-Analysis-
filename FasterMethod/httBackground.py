d7304b625bc6541ccebee907cebd41495c7aa17e
9neeraj0906 <9neerajstudy0906@gmail.com>
2025-12-19 00:43:01 +0530

    This is for second part of background

--- FasterMethod/httBackground.py
+++ FasterMethod/httBackground.py
@@ -12,35 +12,29 @@ from analFxn import Analysis
 
 
 backgroundMCPath = [
-    "/data/bmc/0082C29D-E74C-024A-BE9B-97B29EE7A4A2.root",
-    "/data/bmc/0718C107-8960-6B44-B96A-C60D53D52A95.root",
-    "/data/bmc/0A995C8D-0ACC-7E45-8C7A-16AC51398938.root",
-    "/data/bmc/4578E947-084C-C946-9B8D-1B45A126DCED.root",
-    "/data/bmc/4BD16DC1-5991-6549-9B08-17B67F92A986.root",
-    "/data/bmc/500A8824-5BD4-5741-AECD-FE1A7FD0098D.root",
-    "/data/bmc/52DA0012-7ABA-F94A-A7D3-2FC06E9DF43B.root",
-    "/data/bmc/5544E66D-B271-1649-A85B-C08F01375C57.root",
-    "/data/bmc/69C45E78-12E9-3D41-B5EF-DAF915460237.root",
-    "/data/bmc/6C3CD8A5-A288-724E-BF9F-BAAC46A4C139.root",
-    "/data/bmc/6D7ABB90-DD75-964F-A5CD-14D1B21FAB36.root",
-    "/data/bmc/7169FB77-0571-7641-B0D4-B4270ABF7B39.root",
-    "/data/bmc/74356482-D5C8-D546-85D3-416B9012D25D.root",
-    "/data/bmc/7AAA7AAD-2378-E547-B014-759BBEAA4C42.root",
-    "/data/bmc/9400079E-20CF-1449-BB3C-A7FCDE90F9BF.root",
-    "/data/bmc/94C0AF7B-6727-F246-8E57-384384DADE87.root",
-    "/data/bmc/969B7293-8E74-2A40-8D14-B33204BD6401.root",
-    "/data/bmc/9D579D34-7A90-DF4E-BD6B-A0E9565CF455.root",
-    "/data/bmc/9EDE17FC-CAB6-A146-9714-F1C496F1CBE2.root",
-    "/data/bmc/A17A523B-378F-9A4F-89D6-71C80D612F4E.root"
-
-
-
-
-
-
+    "/data/bmc/18C499E2-9229-DC45-A68F-6A40600A5C8E.root",
+    "/data/bmc/18DA8B20-842D-0741-BAFB-1A1C7C522921.root",
+    "/data/bmc/196F07F8-8EC2-6A48-9036-8B7804B71A1F.root",
+    "/data/bmc/1D85E077-414B-0349-A901-CD6B3B3C349D.root",
+    "/data/bmc/226E10F4-29BC-CF45-B8BF-31B13B4EC1CE.root",
+    "/data/bmc/4046329B-AF7C-6142-9B76-7EE37FB93DCD.root",
+    "/data/bmc/440A3E55-6EE5-3642-B16C-F7FC5FAB5081.root",
+    "/data/bmc/A2058F3B-C2A2-4E43-B963-6C5A4DC8084B.root",
+    "/data/bmc/A20C5B1A-2CFF-7B42-82A4-06347D0856A7.root",
+    "/data/bmc/AE5A627E-C8D7-974F-9698-C136D2BFFF1D.root",
+    "/data/bmc/AF650968-C8A8-9043-9BBB-F83AE7726FEE.root",
+    "/data/bmc/C3A045B1-F50D-C540-8AD5-2F32956D4AA1.root",
+    "/data/bmc/D05EF786-4706-D045-A152-4A81F06EDB04.root",
+    "/data/bmc/D20B2931-4C27-FF4E-B640-3404DFD65150.root",
+    "/data/bmc/D6094FF2-1AAE-784B-9991-AA8441B9FB45.root",
+    "/data/bmc/E0984EEB-ED4B-B84C-BDB7-845E8A2AB85C.root",
+    "/data/bmc/E215B4B9-11EA-D84F-A6D7-9FE57515B2CE.root",
+    "/data/bmc/E82ACE74-60DA-DC41-A2B0-EF750C4AB5E5.root",
+    "/data/bmc/E94099CB-E3C8-EB43-B182-4EA89C7C5411.root",
+    "/data/bmc/ED08B6D8-2823-D24D-B1BA-1141EA893E7B.root"
 
 ]
-output_file = ROOT.TFile("hMtBackground_combinedPart1.root", "RECREATE")
+output_file = ROOT.TFile("hMtBackground_combinedPart2.root", "RECREATE")
 hMtBackgroundTotal = None
 
 
